800b179235914f3463fea2cb37fd9c3adb050184
Alex Pagan <alexrpagan@gmail.com>
2012-12-04 21:10:39 -0500

    Add logging to doit.

--- settings-files/doit-settings.py
+++ settings-files/doit-settings.py
@@ -102,3 +102,53 @@ INSTALLED_APPS = (
 #STATICFILES_DIRS = ('{{{VEROOT}}}/apps/doit/www/static',)
 
 EXPERTSRC_URL = 'http://{{{SERVER_NAME}}}:{{{EXPERTSRC_PORT}}}'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+        'logfile': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': "{{{VEROOT}}}/log/doit_debug.log",
+            'maxBytes': 50000,
+            'backupCount': 2,
+            'formatter': 'standard',
+        },
+        'console':{
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'WARN',
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'tamer': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+        },
+        'doit': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+        },
+    }
+}
